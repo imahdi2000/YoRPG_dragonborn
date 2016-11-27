b0cad632a93965fa3d163c4666a6619218322975
A11ard <apeng@stuy.edu>
2016-11-27 16:10:04 -0500

    Update Archer.java

--- Archer.java
+++ Archer.java
@@ -7,7 +7,7 @@ HW 35 -- Ye Olde Role Playing Game, Realized
 public class Archer extends Character{
 
     private String name;
-    
+	
     public Archer(){
 	name = "unknown";
 	life = 125;
@@ -23,14 +23,15 @@ public class Archer extends Character{
     public void normalize(){
 	while (count > 0){//funtion returns stat back to the original base stat
 	    defense = defense + 10;
-	    attack = attack - .5;
+	    //  attack = attack - .5;
 	    count -= 1;//decreases the counter
 	}
     }
     public void specialize(){
+       	System.out.println("Thou hast shot a poisoned arrow at Monster!");         
 	count += 1;//counter increaes by 1
 	defense = defense - 10; 
-	attack = attack + .5;	
+	//	attack = attack + .5;	
     }
     public  String about(){
 	return "Archer - Robin Hood Y'all - (Life = 110  Defense = 15  Attack = .5 Strength = 100)";
